5938d9f94d3ab58f806034ada1e01b396df4a1f1
Roger Lamothe <Roger.Lamothe.Jr@gmail.com>
2020-08-05 16:51:27 -0400

    updating project3

--- api.js
+++ api.js
@@ -1,18 +1,25 @@
 const axios = require("axios");
+//To pull api key from .env
+const dotenv = require('dotenv');
+dotenv.config();
 
 axios({
     "method":"GET",
+    "url":Process.env.API_URL,
     "headers":{
-    "content-type":"application/octet-stream",
-    "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
-    "useQueryString":true
-    },"params":{
-    "comparisons":"%5EGDAXI%2C%5EFCHI",
-    "region":"US",
-    "lang":"en",
-    "symbol":"HYDR.ME",
-    "interval":"5m",
-    "range":"1d"
+      "content-type":"application/octet-stream",
+      "x-rapidapi-host":"apidojo-yahoo-finance-v1.p.rapidapi.com",
+      "x-rapidapi-key":process.env.API_KEY,
+      "useQueryString":true
+    },
+    "params":
+    {
+      "comparisons":"%5EGDAXI%2C%5EFCHI",
+      "region":"US",
+      "lang":"en",
+      "symbol":"HYDR.ME",
+      "interval":"5m",
+      "range":"1d"
     }
 })
     .then((response)=>{
