5c975ee9f316d7902537b65b752cca7ba9da1564
Roger Lamothe <Roger.Lamothe.Jr@gmail.com>
2020-08-24 13:13:58 -0400

    remove extra bootstrip

---
+++ client/src/pages/Home.js
@@ -0,0 +1,15 @@
+<div class="container landingPageContainer">
+  <div class="introImage center"></div>
+  <h1 class="introText uperLeft">Capital-Revenue!</h1>
+  <div class="container center">
+    <h2 class="introText center">Financial Freedom!</h2>
+
+    <p class="introText center">Would you like to logIn</p>
+  </div>
+ 
+    <button class="btn waves-effect waves-light" type="submit" id="login-submit" name="action">Submit
+                <i class="material-icons right">logIn</i>
+    </button>
+
+
+</div>
