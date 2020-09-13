5c3137de5214a67c01738b024195891b81561d6c
Mark <mark.litfin@yahoo.com>
2020-09-13 00:24:07 -0400

    added the single stock search functionality

--- client/src/pages/Investments.js
+++ client/src/pages/Investments.js
@@ -2,12 +2,14 @@ import React from "react"
 import InvestmentList  from "../component/InvestmentList";
 import Logout from "../component/Logout";
 import Dash from "../component/Backtodash"
+import SearchStocks from "../component/StockSearch";
 
 function DashBoard() {
     return(
 
     <div className="DashBoard">
         
+        <SearchStocks />
         <InvestmentList />
         <Dash />
         <Logout />
